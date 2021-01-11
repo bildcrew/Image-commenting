d8f2fe421251267222317951393f8ba61a6aac67
Akshay Hemant <akshayhemant@hotmail.com>
2021-01-11 13:06:33 +0530

    Functionality implemented

--- server/api/image.js
+++ server/api/image.js
@@ -11,28 +11,58 @@ router.get('/', async (req, res) => {
     return res.status(500).send('Bad request');
   }
 });
-// @GET api/image/:id
-// @desc get image by id
+// @GET api/image/:name
+// @desc get bubbles of image by name
 // public access
 router.get('/:name', async (req, res) => {
   try {
-    const image = await Images.find({ name: req.params.name });
-    return res.status(200).json({ image });
+    const image = await Images.findOne({ name: req.params.name });
+    console.log(image);
+    return res.status(200).json({ bubbles: image.bubbles });
   } catch (error) {
     return res.status(500).send('Bad request');
   }
 });
+// @PUT api/image/:id
+// @desc creating a bubble
+// public access
+router.post('/:name', async (req, res) => {
+  try {
+    const image = await Images.findOne({ name: req.params.name });
+    image.bubbles.push({
+      id: req.body.bubbleId,
+      comments: req.body.comments,
+      x: req.body.x,
+      y: req.body.y,
+    });
+    await image.save();
+    return res.status(200).send(image.bubbles.pop());
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+// @PUT api/image/:id
+// @desc updating comments of image bubble
+// public access
 router.put('/:name', async (req, res) => {
   try {
-    const { message } = req.body;
+    // return res.status(200);
     const image = await Images.findOne({ name: req.params.name });
-    image.messages.push({ text: message });
+    if (image.bubbles.length > 0)
+      image.bubbles = image.bubbles.map((bubble) => {
+        if (bubble.id === req.body.bubbleId) {
+          bubble.comments.push({ text: req.body.comment });
+        }
+        return bubble;
+      });
+    else console.log('nothing');
     await image.save();
-    return res.status(200).send({ image });
+    return res.status(200).send({ bubbles: image.bubbles });
   } catch (error) {
     console.log(error);
     return res.status(500).send('Bad request');
   }
 });
+
 module.exports = router;
--- server/models/Images.js
+++ server/models/Images.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 
 const imageSchema = new mongoose.Schema({
+  name: { type: String },
   bubbles: [
     {
-      id: { type: Number },
+      id: { type: String },
+      x: { type: Number },
+      y: { type: Number },
       comments: [
         {
           text: String,
         },
       ],
     },
+    { unique: true },
   ],
 });
+
+module.exports = mongoose.model('images', imageSchema, 'images');
